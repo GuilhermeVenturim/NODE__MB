58678ede45602f11b497d059ae6740d2062acd98
Guilherme Venturim <guilhermeventurim@outlook.com>
2022-10-08 10:25:54 -0300

    Atualizando Dados para a view.

--- M7/03_Dados_para_View/index.js
+++ M7/03_Dados_para_View/index.js
@@ -8,7 +8,15 @@ app.set('view engine', 'handlebars')
 
 //Rota home
 app.get('/', (req, res) => {
-    res.render('home')
+
+    const user = {
+        name: "Guilherme",
+        surname: "Venturim",
+        age: "31",
+        profission: "Programador"
+    }
+
+    res.render('home', {user: user})
 })
 
 app.listen(3000, () => {
