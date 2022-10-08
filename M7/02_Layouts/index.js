730e4ea3ab2018cb232d46b850109674e64b3184
Guilherme Venturim <guilhermeventurim@outlook.com>
2022-10-08 10:12:15 -0300

    Layouts, enviando dados para a view

---
+++ M7/02_Layouts/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+//Rota home
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(3000, () => {
+    console.log(`App rodando na porta 3000.`)
+})
+
+
+
+
